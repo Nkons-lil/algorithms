3c05526b6e29fa92ff87b72304d37ca146c6b4ff
Petr Puzanov <peta12321@gmail.com>
2025-11-13 18:24:00 +0900

    RemoveDuplicatesfromSortedArray26 RemoveElement27

---
+++ src/easy/RemoveDuplicatesfromSortedArray26.java
@@ -0,0 +1,24 @@
+package easy;
+
+//https://leetcode.com/problems/remove-duplicates-from-sorted-array/
+public class RemoveDuplicatesfromSortedArray26 {
+    public static int removeDuplicates(int[] nums) {
+        if (nums.length == 0) return 0;
+
+        int i = 1;
+
+        for (int j = 1; j < nums.length; j++) {
+            if (nums[j] != nums[i - 1]) {
+                nums[i] = nums[j];
+                i++;
+            }
+        }
+
+        return i;
+    }
+
+    public static void main(String[] args) {
+        int[] a = {1,2,2,2,2,3,4,4,4,4,4};
+        removeDuplicates(a);
+    }
+}
