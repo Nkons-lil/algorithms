4887e1caf047fc5c4ea018a43a69eb0818105fa7
NadyaZakharova <nkonszakharova@gmail.com>
2025-11-19 20:51:11 +0900

    N58LengthOfLastWord

---
+++ src/N58LengthOfLastWord.java
@@ -0,0 +1,21 @@
+//https://leetcode.com/problems/length-of-last-word/description/
+public class N58LengthOfLastWord {
+    public int lengthOfLastWord(String s) {
+        int end = s.length() - 1;
+
+        while (end >= 0 && s.charAt(end) == ' ') {
+            end--;
+        }
+
+        int start = end;
+        while (start >= 0 && s.charAt(start) != ' ') {
+            start--;
+        }
+
+        return end - start;
+    }
+
+    public static void main(String[] args) {
+        new N58LengthOfLastWord().lengthOfLastWord("Hello world");
+    }
+}
