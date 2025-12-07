60a7920686e1d269cb3bced8cc239019d3bd06dc
NadyaZakharova <nkonszakharova@gmail.com>
2025-12-07 09:02:12 +0900

    N1TwoSum N49GroupAnagrams N387FirstUniqueCharacterInAString

---
+++ src/N387FirstUniqueCharacterInAString.java
@@ -0,0 +1,18 @@
+//https://leetcode.com/problems/first-unique-character-in-a-string/description/
+public class N387FirstUniqueCharacterInAString {
+    public int firstUniqChar(String s) {
+        int[] freq = new int[26];
+
+        for (int i = 0; i < s.length(); i++) {
+            freq[s.charAt(i) - 'a']++;
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+            if (freq[s.charAt(i) - 'a'] == 1) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}
