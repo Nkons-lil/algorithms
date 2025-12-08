f00b24b2f54c605e71fd29ef792f8d761a6af1b0
NadyaZakharova <nkonszakharova@gmail.com>
2025-12-08 17:45:47 +0900

    N125ValidPalindrome N146LRUCache N202HappyNumber N643MaximumAverageSubarrayI N704BinarySearch

---
+++ src/N202HappyNumber.java
@@ -0,0 +1,31 @@
+import java.util.HashSet;
+import java.util.Set;
+
+//https://leetcode.com/problems/happy-number/description/
+public class N202HappyNumber {
+    public boolean isHappy(int n) {
+        Set<Integer> visit = new HashSet<>();
+
+        while (!visit.contains(n)) {
+            visit.add(n);
+            n = getNextNumber(n);
+            if (n == 1) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private int getNextNumber(int n) {
+        int output = 0;
+
+        while (n > 0) {
+            int digit = n % 10;
+            output += digit * digit;
+            n = n / 10;
+        }
+
+        return output;
+    }
+}
---
+++ src/N643MaximumAverageSubarrayI.java
@@ -0,0 +1,24 @@
+//https://leetcode.com/problems/maximum-average-subarray-i/description/
+
+public class N643MaximumAverageSubarrayI {
+    public static double findMaxAverage(int[] nums, int k) {
+        int sum = 0;
+        for (int i = 0; i < k; i++) {
+            sum += nums[i];
+        }
+        int maxSum = sum;
+        for (int i = k; i < nums.length; i++) {
+            sum += nums[i] - nums[i - k];
+            maxSum = Math.max(maxSum, sum);
+        }
+        return (double) maxSum / k;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5,6,7,13,5,1};
+        System.out.println(findMaxAverage(arr,4));
+    }
+}
+
+
+
