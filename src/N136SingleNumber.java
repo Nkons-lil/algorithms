8d698a7f98f831d5f42ab5729097d4e3509bdaa3
Petr Puzanov <100192323+PiterPuzanov@users.noreply.github.com>
2026-03-04 20:53:44 +0900

    Update N136SingleNumber.java

--- src/N136SingleNumber.java
+++ src/N136SingleNumber.java
@@ -1,12 +1,24 @@
 //https://leetcode.com/problems/single-number/description/
-public class N136SingleNumber {
-    public int singleNumber(int[] nums) {
-        int res = 0;
+import java.util.HashMap;
+import java.util.Map;
 
+public class N136SingleNumber {
+
+    public int singleNumber(int[] nums) {
+        Map<Integer, Integer> countMap = new HashMap<>();
+
+        // Считаем количество каждого числа
         for (int n : nums) {
-            res ^= n;
+            countMap.put(n, countMap.getOrDefault(n, 0) + 1);
         }
 
-        return res;
+        // Ищем число, которое встретилось 1 раз
+        for (Map.Entry<Integer, Integer> entry : countMap.entrySet()) {
+            if (entry.getValue() == 1) {
+                return entry.getKey();
+            }
+        }
+
+        return -1; // если не найдено (по условию задачи такого быть не должно)
     }
 }
